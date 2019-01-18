6209a1308e41b6e6565dc99c0b704ed352262606
Max Wong <maxpwong@gmail.com>
2019-01-18 15:33:15 -0800

    Getting this thing on git

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+	<head>
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="main.css">
+		
+	</head>
+
+	<body>
+		<div class="container">
+		  <div class="row">
+			<div class="col-md no-float">
+			  <button type="button" class="btn btn-primary">I would like to make a wish</button>
+			</div>
+			<div class="col-md no-float">
+			  <button type="button" class="btn btn-success">I would like to grant a wish</button>
+			</div>
+		  </div>
+		</div>
+	</body>
+
+</html>
+
+
+
+
